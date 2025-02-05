8748e4d4628adc5d0b486dcc3be654bc2f77d557
MeghanshuSanghi <meghanshusanghi@gmail.com>
2025-02-05 15:00:37 +0530

    extra file removed

--- contracts/HelloWorld.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity >=0.5.0 <0.7.0;
-// SPDX-License-Identifier: UNLICENSED
-contract HelloWorld {
-    function get()public pure returns (string memory){
-        return 'Hello World';
-    }
-}
